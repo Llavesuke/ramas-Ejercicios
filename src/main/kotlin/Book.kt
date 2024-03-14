8f6db7cd2bfc2cccff00424f257501a5b9c91c5c
llavesuke <llavesukeprogram@gmail.com>
2024-03-14 16:47:34 +0100

    Ejercicio 1 terminado

--- src/main/kotlin/Book.kt
+++ src/main/kotlin/Book.kt
@@ -1,5 +1,15 @@
 package org.pebiblioteca
 
+/**
+ * Contains all book information
+ *
+ * @property ID An integer number
+ * @property Title The title of the book
+ * @property Author The author name
+ * @property yearOfRelease Year of release of the book
+ * @property Topic The topic of the book
+ * @property State If the book is borrowed or not
+ */
 data class Book(val ID: Int,
                 val title: String,
                 val author: String,
