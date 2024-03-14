5b786db1ba846ac0775f2f76b71054f762e1bc26
llavesuke <llavesukeprogram@gmail.com>
2024-03-14 18:06:04 +0100

    Ejercicio 2 terminado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,24 @@
 package org.pebiblioteca
 
+import jdk.jshell.execution.Util
+
 fun main() {
-    println("Hello World!")
+    val libraryManager = LibraryManager()
+
+    val book1 = Book( title ="Romance of the Three Kingdoms", author = "Luo Guanzhong", yearOfRelease = 1522, topic = "Fiction")
+    val book2 = Book(title = "Initial D", author =  "Suichi Shigeno", yearOfRelease =  1995, topic =  "Races")
+    val book3 = Book(title = "The Hound of the Baskervilles", author = "Arthur Conan Doyle", yearOfRelease =  1902, topic = "Mystery")
+
+    libraryManager.addBookToCatalog(book1)
+    libraryManager.addBookToCatalog(book2)
+    libraryManager.addBookToCatalog(book3)
+
+    libraryManager.registerLoan(book1)
+    libraryManager.registerLoan(book2)
+
+    libraryManager.returnBook(book1)
+    libraryManager.returnBook(book3)
+
+    val bookList = libraryManager.checkBooks()
+    Utils.showList(bookList)
 }
---
+++ src/main/kotlin/Utils.kt
@@ -0,0 +1,58 @@
+package org.pebiblioteca
+
+/**
+ * Where all the inputs are outputs are managed
+ */
+class Utils {
+    companion object{
+        /**
+         * Print the message in the terminal
+         *
+         * @param message The message to be printed
+         */
+        fun showMessage(message: String){
+            println(message)
+        }
+
+        /**
+         * Ask the user a string
+         *
+         * @return The user input
+         */
+        fun stringInput(): String{
+            val input = readln().trim()
+
+            return input
+        }
+
+        /**
+         * Ask a number to the user
+         *
+         * @throws NumberFormatException If the user write something different from an Int
+         * @return The number introduced by the user
+         */
+        fun numberInput(): Int {
+            var number: Int? = null
+
+            while (number == null){
+                try {
+                    print("-> ")
+                    val input = readln().toInt()
+                    number = input
+                } catch (e: NumberFormatException){
+                    showMessage("ERROR: NOT VALID FORMAT")
+                }
+            }
+            return number
+        }
+
+        /**
+         * Show the elements of a list
+         *
+         * @param list The list to show his elements
+         */
+        fun showList(list: List<Book>){
+            list.forEach { println("${it.title}, ${it.author}, ${it.ID}")}
+        }
+    }
+}
